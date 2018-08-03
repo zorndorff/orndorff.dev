761379b5d94ac213bf0e8b06de7829fa7bf2bac3
Zac O <zorndorff@gmail.com>
2018-08-03 11:54:42 -0500

    fix site deployment script

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,11 @@
 BUILD_TIME=`date`
 
 echo Syncing deploy repo changes
-
-cd public
-git init
-git remote add origin git@github.com:zorndorff/zorndorff.github.io.git
-git pull origin master
-
-cd ../
-hugo --cleanDestinationDir
+cp CNAME public/
+hugo
 
 cd public
 git add -A
 git commit -am "Content Update $BUILD_TIME"
+git pull origin master
 git push origin master
